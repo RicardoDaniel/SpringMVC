2ceb827b9cabe00ad244ec77833afcbd72fb4486
Training SQ <Training SQ@AS201811P4317>
2019-05-29 21:07:32 +0100

    -modelação inicial da app

---
+++ preescolarStatic/src/pt/preescolar/entidades/EncarregadoEducação.java
@@ -0,0 +1,5 @@
+package pt.preescolar.entidades;
+
+public class EncarregadoEducação {
+
+}
---
+++ preescolarStatic/src/pt/preescolar/root/preEscolarApp.java
@@ -0,0 +1,10 @@
+package pt.preescolar.root;
+
+public class preEscolarApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
